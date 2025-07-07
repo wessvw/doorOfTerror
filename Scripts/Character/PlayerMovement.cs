8a98ad36bb9e4ef8ffa4ae999f52debc26001d19
Wess <wess.v.wijhe08@gmail.com>
2025-07-07 09:38:53 +0200

    camera moves with mouse

--- Scripts/Character/PlayerMovement.cs
+++ Scripts/Character/PlayerMovement.cs
@@ -7,12 +7,19 @@ public partial class PlayerMovement : CharacterBody3D
 	public const float JumpVelocity = 4.5f;
 	public float speed = 0;
 	Vector3 velocity;
+	public Node3D CameraPivot;
+	public float MouseSensitivity = 0.002f;
+
+	private float pitch = 0.0f;
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		velocity = Velocity;
 
-		// Add the gravity.
 		if (!IsOnFloor())
 		{
 			velocity += GetGravity() * (float)delta;
@@ -23,9 +30,6 @@ public partial class PlayerMovement : CharacterBody3D
 		{
 			velocity.Y = JumpVelocity;
 		}
-		// test
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("left", "right", "forward", "backward");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
@@ -44,12 +48,30 @@ public partial class PlayerMovement : CharacterBody3D
 		MoveAndSlide();
 
 	}
-	public void push(float power, float direction)
+	
+	// mouse camera movement
+	public override void _Input(InputEvent @event)
 	{
-		velocity.X = velocity.X - Mathf.Cos(direction) * power;
-		velocity.Y = Mathf.Sin(direction) * power;
-		Velocity = velocity;
-		MoveAndSlide();
+		if (@event is InputEventMouseMotion motion)
+		{
+			RotateY(-motion.Relative.X * MouseSensitivity);
+
+			pitch -= motion.Relative.Y * MouseSensitivity;
+			pitch = Mathf.Clamp(pitch, -Mathf.Pi / 2, Mathf.Pi / 2);
+
+			if (CameraPivot != null)
+			{
+				CameraPivot.Rotation = new Vector3(pitch, 0, 0);
+			}
+		}
 	}
+
+	// public void push(float power, float direction)
+	// {
+	// 	velocity.X = velocity.X - Mathf.Cos(direction) * power;
+	// 	velocity.Y = Mathf.Sin(direction) * power;
+	// 	Velocity = velocity;
+	// 	MoveAndSlide();
+	// }
 
 }
