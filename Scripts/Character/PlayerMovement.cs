af56d86551dee3383720edf84f651a573c797580
Wess <wess.v.wijhe08@gmail.com>
2025-07-07 10:07:23 +0200

    camerapivot & can check if near door

--- Scripts/Character/PlayerMovement.cs
+++ Scripts/Character/PlayerMovement.cs
@@ -7,13 +7,16 @@ public partial class PlayerMovement : CharacterBody3D
 	public const float JumpVelocity = 4.5f;
 	public float speed = 0;
 	Vector3 velocity;
-	public Node3D CameraPivot;
+	public Node3D cameraPivot;
 	public float MouseSensitivity = 0.002f;
+	private StaticBody3D door;
 
 	private float pitch = 0.0f;
 	public override void _Ready()
 	{
 		Input.MouseMode = Input.MouseModeEnum.Captured;
+		cameraPivot = FindChild("CameraPivot", true, false) as Node3D;
+		door = GetTree().Root.FindChild("door", true, false) as StaticBody3D;
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -42,13 +45,28 @@ public partial class PlayerMovement : CharacterBody3D
 			velocity.X = Mathf.MoveToward(velocity.X, 0, Speed);
 			velocity.Z = Mathf.MoveToward(velocity.Z, 0, Speed);
 		}
+		if (Input.IsActionJustPressed("accept"))
+		{
+			if (door != null && isPlayerNearDoor())
+			{
+				GD.Print("player is near the door");
+			}
+			else
+			{
+				GD.Print("player is not close enough");
+			}
+		}
+		if (Input.IsActionJustPressed("escape"))
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
 
 
 		Velocity = velocity;
 		MoveAndSlide();
 
 	}
-	
+
 	// mouse camera movement
 	public override void _Input(InputEvent @event)
 	{
@@ -57,13 +75,19 @@ public partial class PlayerMovement : CharacterBody3D
 			RotateY(-motion.Relative.X * MouseSensitivity);
 
 			pitch -= motion.Relative.Y * MouseSensitivity;
-			pitch = Mathf.Clamp(pitch, -Mathf.Pi / 2, Mathf.Pi / 2);
+			pitch = Mathf.Clamp(pitch, Mathf.DegToRad(-40), Mathf.DegToRad(40));
 
-			if (CameraPivot != null)
+			if (cameraPivot != null)
 			{
-				CameraPivot.Rotation = new Vector3(pitch, 0, 0);
+				cameraPivot.Rotation = new Vector3(pitch, 0, 0);
 			}
 		}
+	}
+
+	private bool isPlayerNearDoor()
+	{
+		float distance = GlobalPosition.DistanceTo(door.GlobalPosition);
+		return distance <= 5f;
 	}
 
 	// public void push(float power, float direction)
